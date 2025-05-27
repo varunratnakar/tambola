ca136a714506aa4e8f2c84c8a90ef34674aeb320
Varun Ratnakar <varunratnakar@gmail.com>
2025-05-27 22:32:41 +0400

    Fix server url

--- client/src/services/voiceService.js
+++ client/src/services/voiceService.js
@@ -19,7 +19,7 @@ class VoiceService {
     this.cache = new Map();
     
     // Determine API base (dev vs prod)
-    const envBase = import.meta.env.VITE_API_BASE || process.env.REACT_APP_API_BASE;
+    const envBase = import.meta.env.VITE_SERVER_URL || process.env.REACT_APP_API_BASE;
     this.apiBase = envBase ? envBase.replace(/\/$/, '') : this.deriveLocalBase();      
     
     // console.log('VoiceService: Initializing...', {
